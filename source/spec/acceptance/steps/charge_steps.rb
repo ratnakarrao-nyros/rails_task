8e22cae7089acf120642cc53395911271d4338e5
ratnakar <ratnamraju.nyros@gmail.com>
2014-01-21 16:54:19 +0530

    changes in charge steps

--- source/spec/acceptance/steps/charge_steps.rb
+++ source/spec/acceptance/steps/charge_steps.rb
@@ -25,15 +25,15 @@ end
 
 
 step "I should see list of :count successful charges" do |count|
-  successful_charges.count.should == count.to_i
+  @successful_charges.count.should == count.to_i
 end
 
 step "I should see list of :count failed charges" do |count|
- failed_charges.count.should == count.to_i
+  @failed_charges.count.should == count.to_i
 end
 
 step "I should see list of :count dispute charges" do |count|
- dispute_charges.count.should == count.to_i
+  @dispute_charges.count.should == count.to_i
 end
 
 
